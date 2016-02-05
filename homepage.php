9a9c9271f198b94f2e978ab35c34ca1ad629465b
acyao <ac_yao94@hotmail.com>
2016-02-05 15:25:48 +0800

    HOME

---
+++ homepage.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+
+
+<style>
+.register_form
+{
+border-style:solid;
+border-width:1px;
+border-radius:5px;
+background-color:#b3e0ff;
+background-position:center;
+width:300px;
+height:380px;
+margin-left:300px;
+padding-left:40px;
+
+}
+td
+{
+font-size:20px;
+}
+
+ input[type='button']
+{
+	background-position:1px;
+	background-color:#66c2ff;
+	color:#000f1a;
+	border-width:1.5px;
+	border-color:#004c80;
+	border-radius:3px;
+	font-family:arial narrow;
+	font-size:17px;
+}
+
+input[type='button']:hover
+{
+	font-family:arial narrow;
+	background-color:#ccebff;
+	color:#002ecd;
+	border-color:#001f33;
+}
+</style>
+</head>
+<body>
+<div style="border:black 2px solid;height:1122px;width:1020px;margin-left:200px;background-color:#e5f5ff">
+<br/>
+<br/>
+<span style="color:#262626;font-size:55px;font-weight:bold;font-family:arial narrow;padding-left:100px;"><span style="font-style:italic;">P</span>harma<span style="color:#005c99">c</span>y </span><span style="color:#262626;font-size:45px;font-weight:bold;font-family:arial narrow;">Online</span>
+<br/>
+<span style="color:#262626;font-size:25px;font-weight:bold;font-family:arial narrow;padding-left:100px;font-style:oblique;">| The most efficient pharmacy is within your own system</span>
+<br/>
+<br/>
+<br/>
+<div style=";background-color:black; 	height:1px;"></div>
+<div style=";background-color:#004c80;height:40px;"><br/></div>
+<div style=";background-color:black; height:1px;"></div>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="register_form">
+<br/>
+<img src="profilepicture.jpg" title="user" width="120px" style="border-style:solid;border-width:1px;margin-left:67px;margin-bottom:50px;"/>
+<table>
+<tr>
+<td>User Name</td>
+<td>:</td>
+<td><input type="text" name="username"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td>:</td>
+<td><input type="password" name="password"></td>
+</tr>
+<tr>
+<td>login as</td>
+<td>:</td>
+<td><input type="radio" name="type" value="manager">manager<input type="radio" name="type" value="staff">staff</td>
+</tr>
+
+</table>
+<input type="button" name="login" value="login"style="margin-left:75px;margin-top:30px;width:50px;"> <input type="button" name="cancel" value="cancel">
+</div>
+<div style=";background-color:black;height:1px;margin-top:310px;"></div>
+<div style=";background-color:#004c80;height:62px"><br/></div>
+</div>
+
+</body>
+</html>
