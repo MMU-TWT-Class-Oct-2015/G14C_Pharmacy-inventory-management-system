493f5b924bc7d667130787a7f673b784d929b19a
sjting <1121116135@student.mmu.edu.my>
2016-02-12 11:35:37 +0800

    register

--- staff_register.php
+++ staff_register.php
@@ -1,18 +1,4 @@
 <?php
-
-include("connection.php");
-
-// save the session variable in another variable
-$sess_aid = $_SESSION["ad_id"];
-
-// finding the specific member record based on the session variable
-$result = mysql_query("select * from admin where AdminID = $sess_aid");
-$row=mysql_fetch_assoc($result);
-
-if(!isset($_SESSION["ad_id"]))
-{
- header("location:homepage.php");
-}
 if(isset($_POST["btnadd"]))
 {
   $name = $_POST["fullname"];
@@ -27,12 +13,12 @@ if(isset($_POST["btnadd"]))
   mysql_query("insert into staff(StaffName,StaffPassword,StaffUsername,StaffEmail,StaffAge,StaffGender,StaffIC,StaffContactNo,StaffImage) values
   ('$name','$password','$username','$email','$age','$gender','$ic','$contact','Storage/profilepicture.jpg')");
 
-  header("location:admin_homepage.php");
+  header("location:homepage.php");
 }
 
-if(isset($_POST["btncancel"]))
+if(isset($_POST["cancel"]))
 {
-  header("location:admin_homepage.php");
+  header("location:homepage.php");
 }
 ?>
 <!DOCTYPE html>
@@ -40,7 +26,6 @@ if(isset($_POST["btncancel"]))
 <head>
 <title></title>
 
-<link rel="stylesheet" type="text/css" href="css.css"/>
 <style>
 .register_form
 {
@@ -49,9 +34,9 @@ border-width:1px;
 border-radius:5px;
 background-color:#b3e0ff;
 background-position:center;
-width:300px;
-height:380px;
-margin-left:300px;
+width:600px;
+height:600px;
+margin-left:200px;
 padding-left:40px;
 
 }
@@ -60,55 +45,7 @@ td
 font-size:20px;
 }
 
-
-
-.leftprofile
-{
-border:#66c2ff 1px solid;
-height:840px;
-width:280px;
-background:#000f1a;
-
-}
-
-.title
-{
-background-color:#000f1a;
-font-size:25px;
-color:#66c2ff;
-padding:10px;
-font-family:arial narrow;
-border-bottom:#66c2ff solid 1px;
-
-}
-.profile_detail table
-{
-margin-left:30px;
-}
-
-.profile_detail
-{
-background-color:#E6E6E6;
-border:solid 2px ;
-margin-left:300px;
-margin-right:10px;
-margin-top:10px;
-height:700px;
-}
-
-.profile_detail input[type="password"], .profile_detail input[type="text"], .profile_detail input[type="email"], .profile_detail input[type="number"],select, .profile_detail textarea
-{
-	border-style:solid;
-	border-width:2px;
-	border-color:#383838;
-	border-radius:4px;
-	padding-left:40px;
-	margin: 10px 5px;
-	height:27px;
-	width:180px;
-}
-
-.profile_detail input[type='button']
+ input[type='button']
 {
 	background-position:1px;
 	background-color:#66c2ff;
@@ -117,20 +54,16 @@ height:700px;
 	border-color:#004c80;
 	border-radius:3px;
 	font-family:arial narrow;
-	width:120px;
-	font-size:18px;
-	margin-left:10px;
-	margin-top:110px;
+	font-size:17px;
 }
 
-.profile_detail input[type='button']:hover
+input[type='button']:hover
 {
 	font-family:arial narrow;
 	background-color:#ccebff;
 	color:#002ecd;
 	border-color:#001f33;
 }
-
 </style>
 </head>
 <body>
@@ -144,36 +77,24 @@ height:700px;
 <br/>
 <br/>
 <div style=";background-color:black; 	height:1px;"></div>
-<div style=";background-color:#004c80;height:40px;font-weight:bold;color:#ffffff;font-size:15px;"><br/>Admin UserName:<?php echo $row["AdminUsername"]; ?><span style="float:right;">Date:<?php date_default_timezone_set("Asia/Kuala_Lumpur");echo date("d-m-Y H:i:s");?></span></div>
+<div style=";background-color:#004c80;height:40px;"><br/></div>
 <div style=";background-color:black; height:1px;"></div>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
 
-
-<ul class="profile">
-
-<div class="leftprofile">
-<li><a href="admin_homepage.php"><span style="clear:both;">Profile</span></a></li>
-
-<li><a href="admin_editprofile.php"><span style="clear:both;">Edit Profile</span></a>
-</li>
-<li><a href="admin_addstaff.php"><span style="clear:both;">Add Staff</span></a></li>
-<li><a href="admin_additem.php"><span style="clear:both;">Add Item</span></a></li>
-<li><a href="admin_edititem.php"><span style="clear:both;">Edit Item</span></a></li>
-<li><a href="admin_deleteitem.php"><span style="clear:both;">Delete Item</span></a></li>
-<li><a href="admin_viewitem.php"><span style="clear:both;">View Item</span></a></li>
-<li><a href="admin_viewitemsale.php"><span style="clear:both;">View Item sale</span></a></li>
-<li><a href="logout.php"><span style="clear:both;">Log Out</span></a></li>
-
-</div>
-
-</ul>
-
-<div class="profile_detail">
-<div class="title">
-Add Item
-</div>
-<form method="POST" name="AddFrm">
+<div class="register_form">
+<br/>
+<img src="profilepicture.jpg" title="user" width="120px" style="border-style:solid;border-width:1px;margin-left:67px;margin-bottom:50px;"/>
+<form name="registerfrm" method="POST">
 <table>
-  <tr>
+<tr>
   <td><span style="font-weight:bold;">Full Name  </span></td>
   <td>:</td>
   <td><input type="text" name="fullname" placeholder="Your Name"/><span id="one"style="font-size:14px;font-weight:bolder;color:red;"></span></td>
@@ -214,16 +135,11 @@ Add Item
   <td>:</td>
   <td> <input type="text" name="contactNumber" placeholder="Your Contact Number"/><span id="four"style="font-size:14px;font-weight:bolder;color:red;"></span></td>
   </tr>
-<tr>
-<td></td>
-<td></td>
-<td><input type="submit" name="btnadd" value="add" /><input type="submit" name="btncancel" value="Cancel"/></td>
-</tr>
 </table>
+<input type="submit" name="register" value="Register"> <input type="submit" name="cancel" value="cancel"> 
 </form>
 </div>
-
-<div style=";background-color:black;height:1px;margin-top:140px;"></div>
+<div style=";background-color:black;height:1px;margin-top:310px;"></div>
 <div style=";background-color:#004c80;height:62px"><br/></div>
 </div>
 
